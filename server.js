2b33181ae55c4a4fa4e9f31477638f99a2c93eb1
zorochimaru <rasim.karimli@gmail.com>
2024-04-27 14:05:29 +0400

    Updated secret names and env creation script

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const dir = 'src/environments';
-const file = 'environment.ts';
-const prodFile = 'environment.prod.ts'; // For production deployment
+const prodFile = 'environment.prod.ts';
 
-const content = `${process.env.FIREBASE_DETAILS}`;
+const content = `${process.env.ENVIRONMENT_FILE}`;
 
 fs.access(dir, fs.constants.F_OK, err => {
   if (err) {
@@ -21,7 +20,6 @@ fs.access(dir, fs.constants.F_OK, err => {
   }
   // Now write to file
   try {
-    fs.writeFileSync(dir + '/' + file, content);
     fs.writeFileSync(dir + '/' + prodFile, content);
     console.log('Created successfully in', process.cwd());
     if (fs.existsSync(dir + '/' + file)) {
