f7042947bd32f727385ffe8813a4a3680afcadcb
zorochimaru <rasim.karimli@gmail.com>
2024-04-27 13:24:28 +0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,6 @@ import { FooterComponent, HeaderComponent } from './shared';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'test';
+}
