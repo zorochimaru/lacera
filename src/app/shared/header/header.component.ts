82620587fb091e3a6b82c3c6410ec49dd3833965
zorochimaru <rasim.karimli@gmail.com>
2024-05-14 20:07:18 +0400

    Fixed navigation menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,12 @@
-import { JsonPipe, NgFor, NgStyle } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { NgFor, NgStyle } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  OnInit
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
@@ -9,49 +16,28 @@ import { LangCodes, routerAnchorLinks, routerLinks } from '../../core';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    TranslocoModule,
-    RouterModule,
-    FormsModule,
-    NgFor,
-    NgStyle,
-    JsonPipe
-  ],
+  imports: [TranslocoModule, RouterModule, FormsModule, NgFor, NgStyle],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
   #translocoService = inject(TranslocoService);
+  #elRef = inject(ElementRef);
+
+  @HostBinding('class.scrolled') scrolled = false;
+
+  @HostListener('document:scroll', ['$event'])
+  onWindowScroll() {
+    const boundingHeader = this.#elRef.nativeElement.getBoundingClientRect();
+    const scrollTop = document.documentElement.scrollTop;
+    this.scrolled = scrollTop > boundingHeader.top;
+  }
 
   protected langCodes = Object.values(LangCodes);
   protected routerLinks = routerLinks;
   protected routerAnchorLinks = routerAnchorLinks;
   protected selectedLang = '';
-  // @HostBinding('class.not-top') notTop = false;
 
-  // ⤵️ Access through ViewChild like this
-  // @ViewChild('nav') header!: ElementRef;
-
-  // @HostListener('document:scroll', ['$event'])
-  // public onViewportScroll() {
-  //   // ⤵️ Captures / defines current window height when called
-  //   const windowHeight = window.innerHeight;
-  //   // ⤵️ Captures bounding rectangle of 5th element
-
-  //   const boundingHeader = this.header.nativeElement.getBoundingClientRect();
-
-  //   // ⤵️ IF the top of the element is greater or = to 0 (it's not ABOVE the viewport)
-  //   // AND IF the bottom of the element is less than or = to viewport height
-  //   // show the corresponding icon after half a second
-  //   // else hide all icons
-  //   if (boundingHeader.top >= 0 && boundingHeader.bottom <= windowHeight) {
-  //     setTimeout(() => {
-  //       this.notTop = false;
-  //     }, 500);
-  //   } else {
-  //     this.notTop = true;
-  //   }
-  // }
   public ngOnInit(): void {
     this.selectedLang = this.#translocoService.getActiveLang();
   }
