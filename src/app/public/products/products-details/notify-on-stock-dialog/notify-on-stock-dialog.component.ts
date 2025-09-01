26d8810469fa8c8f7605fa6f06b77f41480da7ba
zorochimaru <rasim.karimli@gmail.com>
2025-09-01 18:27:56 +0400

    feat: refactor notify-on-stock-dialog component to use reactive forms, enhance validation, and improve UI for contact inputs

--- src/app/public/products/products-details/notify-on-stock-dialog/notify-on-stock-dialog.component.ts
+++ src/app/public/products/products-details/notify-on-stock-dialog/notify-on-stock-dialog.component.ts
@@ -1,44 +1,91 @@
 import { DialogRef } from '@angular/cdk/dialog';
 import { Component, inject } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TranslocoDirective } from '@jsverse/transloco';
-import { NgxMaskDirective } from 'ngx-mask';
+import { IntlTelInputComponent } from 'intl-tel-input/angularWithUtils';
 
 import { routerLinks } from '../../../../core';
 
 @Component({
-    selector: 'app-notify-on-stock-dialog',
-    imports: [
-        ReactiveFormsModule,
-        TranslocoDirective,
-        RouterLink,
-        NgxMaskDirective
-    ],
-    templateUrl: './notify-on-stock-dialog.component.html',
-    styleUrl: './notify-on-stock-dialog.component.scss'
+  selector: 'app-notify-on-stock-dialog',
+  imports: [
+    ReactiveFormsModule,
+    TranslocoDirective,
+    RouterLink,
+    IntlTelInputComponent
+  ],
+  templateUrl: './notify-on-stock-dialog.component.html',
+  styleUrl: './notify-on-stock-dialog.component.scss'
 })
 export class NotifyOnStockDialogComponent {
   readonly #dialogRef = inject(DialogRef);
+  readonly #fb = inject(FormBuilder);
 
-  protected customerPhoneNumber = new FormControl('+994', {
-    nonNullable: true
-  });
-  protected customerName = new FormControl('', {
-    nonNullable: true,
-    validators: [Validators.required]
-  });
-  protected termsAcceptControl = new FormControl(false, {
-    validators: [Validators.requiredTrue],
-    nonNullable: true
-  });
+  protected isPhoneValid = false;
+
+  protected contactValidator = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const email = control.get('customerEmail')?.value;
+    const phone = control.get('customerPhoneNumber')?.value;
+
+    // Quick check - if we have a valid phone, don't validate further
+    if (this.isPhoneValid && phone && phone.length > 5) {
+      return null;
+    }
+
+    // Only check email if phone is not valid
+    const isEmailValid = email && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    if (!isEmailValid) {
+      return { contactRequired: true };
+    }
+
+    return null;
+  };
+
+  protected contactForm = this.#fb.group(
+    {
+      customerPhoneNumber: this.#fb.control('+994', {
+        nonNullable: true
+      }),
+      customerEmail: this.#fb.control('', {
+        nonNullable: true
+      }),
+      customerName: this.#fb.control('', {
+        validators: [Validators.required],
+        nonNullable: true
+      }),
+      termsAccept: this.#fb.control(false, {
+        validators: [Validators.requiredTrue],
+        nonNullable: true
+      })
+    },
+    { validators: [this.contactValidator] }
+  );
 
   protected routerLinks = routerLinks;
 
+  protected handleValidityChange(isValid: boolean): void {
+    this.isPhoneValid = isValid;
+    // Form validation will be triggered automatically when needed
+  }
+
   protected placeNotification(): void {
+    const { customerName, customerPhoneNumber, customerEmail } =
+      this.contactForm.getRawValue();
+
     this.#dialogRef.close({
-      customerPhoneNumber: this.customerPhoneNumber.getRawValue(),
-      customerName: this.customerName.getRawValue()
+      customerPhoneNumber,
+      customerName,
+      customerEmail
     });
   }
 }
